92f69620c1dbbfce753abb7ecdd9bc2c9e5accfc
nurdandehoust941 <nurdandehoust941@gmail.com>
2025-03-18 10:17:51 +0000

    Add 14.js

---
+++ src/14.js
@@ -0,0 +1,9 @@
+// This is an example of a simple function that takes two arguments and returns their sum
+function add(a, b) {
+  return a + b;
+}
+
+// Test the function with some examples
+console.log(add(2, 3)); // Output: 5
+console.log(add(-1, 4)); // Output: 3
+console.log(add(0, 10)); // Output: 10
